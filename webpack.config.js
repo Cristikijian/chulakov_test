f58945e6c92e2e7a0340e7fbb7a07dd178bbbbba
Cristikijian <cristikijian@gmail.com>
2024-03-08 12:09:10 +0300

    basic html structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const env = process.env.NODE_ENV || "development";
 
 module.exports = {
   entry: {
-    main: './src/index.js',
-
+    main: "./src/index.js",
   },
+  mode: env,
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+      watch: true,
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
     new HtmlWebpackInlineSVGPlugin(),
-    new CopyPlugin({
-      patterns: [
-        { from: path.resolve(__dirname, 'public'), to: path.resolve(__dirname, 'dist') },
-      ],
-    })
-  ]
+    env === "production"
+      ? new CopyPlugin({
+          patterns: [
+            {
+              from: path.resolve(__dirname, "public"),
+              to: path.resolve(__dirname, "dist"),
+            },
+          ],
+        })
+      : undefined,
+  ],
 };

adfa6a5e2b598d23479be2c83a3998a3ea5786f6
Cristikijian <cristikijian@gmail.com>
2024-03-08 14:30:00 +0300

    webpack config with sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: {
@@ -13,18 +14,38 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+
   },
   devServer: {
+    watchFiles: path.resolve(__dirname, "src"),
     static: {
       directory: path.resolve(__dirname, "public"),
       watch: true,
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css)$/,
+        use: [
+          env !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
     new HtmlWebpackInlineSVGPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
     env === "production"
       ? new CopyPlugin({
           patterns: [
